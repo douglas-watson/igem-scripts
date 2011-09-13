9d3db28b00d3cc43e2b14d8fd1f1278bc2ec2df8
Douglas Watson <douglas@watsons.ch>
2011-09-13 10:48:35 +0200

    [plate-reader] Configurable plotting works

--- plate-reader/plate-reader.R
+++ plate-reader/plate-reader.R
@@ -16,55 +16,103 @@
 
 library('ggplot2')
 library('reshape')
+library('chron')
 
 ###############################
 # Load data
 ###############################
-fluo = read.table('data/05-09-cotransfs-fluo.dat', header=TRUE, sep='\t')
-samplenames = read.table('config/samplenames.conf', header=TRUE, sep='\t')
-titles = read.table('config/titles.conf', header=TRUE, sep='\t')
+args = commandArgs(trailingOnly = TRUE)
+
+fluo = read.table(args[1], header=TRUE, sep='\t')
+od = read.table(args[2], header=TRUE, sep='\t')
+samplenames = read.table(args[3], header=TRUE, sep='\t')
+titles = read.table(args[4], header=TRUE, sep='\t')
 
 # Rename column names to sample names, convert times to time objects
 names(fluo)[-1] = sapply(samplenames$SampleName, toString)
+names(od)[-1] = sapply(samplenames$SampleName, toString)
 # Convert time to POSIX datetime objects. Introduces fake date so it works well.
 fluo$Time = as.POSIXct(sapply(fluo$Time, toString), format="%H:%M:%S")
+od$Time = as.POSIXct(sapply(od$Time, toString), format="%H:%M:%S")
+
+# Add an hours column
+fluo$Hours = as.integer(format(fluo$Time, "%H"))
+od$Hours = as.integer(format(od$Time, "%H"))
 
 ###############################
 # Helper Functions
 ###############################
 
-comparison_plot = function(data, melted_data) {
+colour_comparison_plot = function(data, molten_data, ylabel="Fluorescence") {
 	# Make plot with title data$PlotTitle containing the fluorescence intensity
 	# data for the corresponding data$SampleName, from the fluo.m data frame.
 	# Values for each sample are plotted as a smooth curve, distinguished by
 	# their colour.
 
 	maintitle = data$PlotTitle[1]
-	subdata = subset(melted_data, SampleName %in% data$SampleName)
-
+	subdata = subset(molten_data, SampleName %in% data$SampleName)
 	# For proper colouring, the SampleName factor must be redefined:
 	subdata$SampleName = factor(subdata$SampleName)
-	p = qplot(Time, value, data=subdata, colour=SampleName, geom='smooth',
-		main=maintitle, ylab='Fluorescence') +	
+
+	p = qplot(Time, avg, data=subdata, colour=SampleName, 
+		main=maintitle, ylab=ylabel, 
+		xlim=c(min(subdata$Time), max(subdata$Time))) +	
+		geom_errorbar(aes(ymin=avg - std, ymax=avg + std)) +
 		scale_x_datetime(major='2 hours', format='%H h') +
-		scale_colour_hue('Sample Name', breaks=data$SampleName, 
-			labels=sub('-', ' ', data$SampleName))
-	ggsave(paste('plots/', maintitle, '.pdf', sep=''), width=16, height=10)
+		scale_colour_hue('Sample Name')
+	ggsave(paste('plots/', maintitle, '_colour.pdf', sep=''), width=16, height=10)
 	return(p)
 }
 
+facet_comparison_plot = function(data, molten_data, ylabel="Fluorescence") {
+	# Make plot with title data$PlotTitle containing the fluorescence intensity
+	# data for the corresponding data$SampleName, from the fluo.m data frame.
+	# Values for each sample are plotted as a smooth curve, distinguished by
+	# their colour.
+
+	maintitle = data$PlotTitle[1]
+	subdata = subset(molten_data, SampleName %in% data$SampleName)
+	# For proper colouring, the SampleName factor must be redefined:
+	subdata$SampleName = factor(subdata$SampleName)
+
+	p = qplot(Time, avg, data=subdata, facets= SampleName ~ ., 
+		main=maintitle, ylab=ylabel, 
+		xlim=c(min(subdata$Time), max(subdata$Time))) +	
+		geom_errorbar(aes(ymin=avg - std, ymax=avg + std)) +
+		scale_x_datetime(major='2 hours', format='%H h')
+	ggsave(paste('plots/', maintitle, '_facets.pdf', sep=''), width=16, height=10)
+	return(p)
+}
+
+
 ###############################
-# Plotting
+# Calculations and Plotting
 ###############################
 
-fluo.m = melt(fluo, id.vars='Time', variable_name='SampleName')
+fluo.m = melt(fluo, id.vars=c('Time', 'Hours'), 
+	variable_name='SampleName')
+od.m = melt(od, id.vars=c('Time', 'Hours'), 
+	variable_name='SampleName')
 
-# Plot fluorescence versus time, facetted by sample type
+combined.m = data.frame(Time=fluo.m$Time, Hours=fluo.m$Hours,
+	SampleName=fluo.m$SampleName, fluo=fluo.m$value, od=od.m$value)
+
+
+# Plot raw fluorescence versus time, facetted by sample type
 p = qplot(Time, value, data = fluo.m) + 
 	facet_wrap(~ SampleName, ncol=12) +
 	scale_x_datetime(major='4 hours', format="%Hh")
+# ggsave("plots/facet_plot_F.pdf", width=24, height=15)
 
+# And the same for optical density
+n = qplot(Time, value, data = od, ylab = "Optical Density") +
+	facet_wrap(~ SampleName, ncol=12)
+# ggsave("plots/facet_plot_OD.pdf", width=24, height=14)
 
-# Comparison plots, as specified in config/titles.conf
-d_ply(titles, .variables=('PlotTitle'), .fun=comparison_plot, melted_data=fluo.m, .print=FALSE)
+# Calculate average fluorescence per hours, normalised by OD
+normalised = ddply(combined.m, .variables=c('SampleName', 'Hours'), 
+	.fun=summarise, avg=mean(fluo)/mean(exp(od)), std=sd(fluo/exp(od)))
+normalised$Time = as.POSIXct(sapply(normalised$Hours, toString), format="%H")
 
+d_ply(titles, .variables=('PlotTitle'), .fun=facet_comparison_plot, 
+		molten_data=normalised, ylabel="Normalised Fluorescence [a.u.]")
---
+++ plate-reader/vincents-plots.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+R -f plate-reader.R --args data/11_09-lysis-final-fluo.dat data/11_09-lysis-final-OD.dat config/t7/lysis_final_config.dat config/t7/titles_lysis
+
