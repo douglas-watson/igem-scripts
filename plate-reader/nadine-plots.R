98a6db543a574e7747e93b0d0ddd723337ef1f63
Douglas Watson <douglas@watsons.ch>
2011-09-21 01:40:59 +0200

    [plate-reader] Separated nadine's graphs. Made the tetR graphs

---
+++ plate-reader/nadine-plots.R
@@ -0,0 +1,251 @@
+#!/usr/bin/env R -f
+#
+# nadine-plots.R - Nadine's plots
+#
+# Author:
+# 	Douglas Watson <douglas@watsons.ch>
+#
+# Date:
+# 	Continued on 20th September 2011
+# 
+# License:
+# 	GNU GPL v3.0
+#
+##########################################
+
+library('ggplot2')
+library('reshape')
+library('chron')
+
+################################
+# Nadine's experiments
+################################
+
+od = read.table('data/16.09-nadine-OD.dat', header=TRUE, sep='\t')
+rfu = read.table('data/16.09-nadine-RFU.dat', header=TRUE, sep='\t')
+samplenames = read.table('config/16.09-nadine.titles', header=TRUE, sep='\t')
+
+# Melt and merge, convert time, insert sample concentration and name
+od.m = melt(od, id=c('Time'), variable_name='Code')
+rfu.m = melt(rfu, id=c('Time'), variable_name='Code')
+both.m = data.frame(Time=od.m$Time, Code=od.m$Code, OD=od.m$value,
+	RFU=rfu.m$value)
+both.m$Time = as.POSIXct(sapply(both.m$Time, toString), format="%H:%M:%S")
+both.m = merge(both.m, samplenames)
+
+# Normalise RFU, calculate average over the last hour (keep repeats separate)
+both.m$NormRFU = with(both.m, RFU/OD)
+
+# calculate last hour averages, and repeat averages
+averages = ddply(both.m, .variables=c('Time', 'SampleName', 'Concentration'),
+	.fun=summarise, avg=mean(NormRFU), std=sd(NormRFU)/sqrt(2))
+both.m$Hours = as.integer(format(both.m$Time, "%H"))
+last_hour_averages = ddply(subset(both.m, Hours == max(Hours) - 1), 
+		.variables=c('SampleName', 'Concentration', 'Repeat'), .fun=summarise,
+		avg=mean(NormRFU), std=sd(NormRFU)/sqrt(6))
+
+# Now separate experiments
+# Current numbering: (output of levels(samplenames$SampleName))
+# [1] "J61002 Plac-RFP + IPTG"      
+# [2] "J61002 Ptet-RFP + ATC"        
+# [3] "J6-pLac-RFP in BL21 + IPTG"      
+# [4] "pSB3K1-pConst-TetR + J6-pTet-RFP + ATC"                      
+# [5] "pSB3K1 Pconst-TetR Ptet-LacI + J61002 Plac-RFP + ATC"   
+# [6] "pSB3K1 Pconst-TetR Ptet-LacI + J61002 Plac-RFP + IPTG"         
+
+print(levels(samplenames$SampleName))
+for ( i in seq(1, 6) ) {
+	# Assign variables expN, expN.av, and expN.lh
+	assign(paste('exp', i, sep=''), subset(both.m, 
+			SampleName == levels(SampleName)[i]))
+	assign(paste('exp', i, '.av', sep=''), subset(averages, 
+			SampleName == levels(SampleName)[i]))
+	assign(paste('exp', i, '.lh', sep=''), subset(last_hour_averages, 
+			SampleName == levels(SampleName)[i]))
+}
+
+# Dynamics plots:
+dynplot = function(all_data, av_data, colourlabel, clpalette='YlOrRd', 
+	smooth=FALSE) {
+	maintitle = toString(unique(all_data$SampleName))
+	all_data$Concentration = factor(all_data$Concentration)
+	av_data$Concentration = factor(av_data$Concentration)
+	dyn = qplot(Time, NormRFU, data = all_data, geom = 'point',
+		colour = Concentration,
+		xlab = "Time", ylab = "Normalised RFU [a.u.]", main=maintitle) +
+		#geom_smooth(method=loess, n=10) +
+		scale_colour_brewer(colourlabel, palette = clpalette) + 
+		scale_x_datetime(minor = '10 min', format = '%H h')
+	if ( smooth == TRUE ) {
+		dyn = dyn + geom_smooth()
+	}
+	return(dyn)
+}
+
+dyn_exp1 = dynplot(exp1, exp1.av, "Conc. [µM]", smooth=TRUE)
+dyn_exp1
+
+exp1$Concentration = factor(exp1$Concentration)
+qplot(OD, RFU, data = exp1, colour= Concentration, log='y') +
+	scale_colour_brewer(palette="YlOrRd")
+qplot(Time, OD, data = exp1, colour=Concentration) +
+	scale_colour_brewer(palette="YlOrRd")
+max(exp1$OD)
+
+# Dynamics and dose response plot for exp2:
+dosplot = function(lh_data, xunit='[µM]') {
+	maintitle = toString(unique(lh_data$SampleName))
+	xbreaks = unique(lh_data$Concentration)[-2]
+	p = qplot(Concentration, avg, data=lh_data, geom='point',
+		colour=factor(Repeat),
+		xlab = paste("Conc.", xunit), log = 'x', 
+		ylab="Normalised RFU at saturation [a.u]", main=maintitle )  +
+		geom_linerange(aes(ymin=avg-1.96*std, ymax=avg+1.96*std)) +
+		scale_x_continuous(breaks = xbreaks) +
+		scale_colour_hue("Repeat")
+	return(p)
+}
+
+# Make both plots for all experiments
+dyn_exp1 = dynplot(exp1, exp1.av, "Conc. [µM]", smooth=TRUE)
+dyn_exp2 = dynplot(exp2, exp2.av, "Conc. [ng / ml]", clpalette="Spectral")
+dyn_exp3 = dynplot(exp3, exp3.av, "Conc. [µM]")
+dyn_exp4 = dynplot(exp4, exp4.av, "Conc. [ng / ml]", clpalette="Spectral",
+	smooth=TRUE)
+dyn_exp5 = dynplot(exp5, exp5.av, "Conc. [ng / ml]",
+	smooth=TRUE)
+dyn_exp6 = dynplot(exp6, exp6.av, "Conc. [µM]",
+	smooth=TRUE)
+
+dos_exp1 = dosplot(exp1.lh)
+dos_exp2 = dosplot(exp2.lh, xunit='[ng/ml]')
+dos_exp3 = dosplot(exp3.lh)
+dos_exp4 = dosplot(exp4.lh, xunit='[ng/ml]')
+dos_exp5 = dosplot(exp5.lh, xunit='[ng/ml]')
+dos_exp6 = dosplot(exp6.lh)
+
+# And save them
+for ( i in seq(1, 6) ) { 
+	ggsave(paste("plots/nadine-exp", i, "-dynamics.pdf", sep=''), 
+		plot=get(paste('dyn_exp', i, sep='')), width=16, height=10)
+	ggsave(paste("plots/nadine-exp", i, "-doseresponse.pdf", sep=''), 
+		plot=get(paste('dos_exp', i, sep='')), width=16, height=10)
+}
+
+######################################
+# TetR mutants
+######################################
+
+od = read.table('data/20.09-tetrmutants-OD.dat', header=TRUE, sep='\t')
+rfu = read.table('data/20.09-tetrmutants-RFU.dat', header=TRUE, sep='\t')
+samplenames = read.table('config/20.09-tetR.titles', header=TRUE, sep='\t')
+
+# Melt and merge, convert time, insert sample concentration and name
+od.m = melt(od, id=c('Time'), variable_name='Code')
+rfu.m = melt(rfu, id=c('Time'), variable_name='Code')
+both.m = data.frame(Time=od.m$Time, Code=od.m$Code, OD=od.m$value,
+	RFU=rfu.m$value)
+both.m$Time = as.POSIXct(sapply(both.m$Time, toString), format="%H:%M:%S")
+both.m = merge(both.m, samplenames)
+
+# Normalise RFU, 
+both.m$NormRFU = with(both.m, RFU/OD)
+
+# calculate last hour averages, and repeat averages
+averages = ddply(both.m, .variables=c('Time', 'SampleName', 'Concentration'),
+	.fun=summarise, avg=mean(NormRFU), std=sd(NormRFU)/sqrt(length(NormRFU)))
+both.m$Hours = as.integer(format(both.m$Time, "%H"))
+last_hour_averages = ddply(subset(both.m, Hours == max(Hours) - 1), 
+		.variables=c('SampleName', 'Concentration', 'Repeat'), .fun=summarise,
+		avg=mean(NormRFU), std=sd(NormRFU)/sqrt(length(NormRFU)))
+
+
+# Separate by mutants: 
+# [1] "E37AW43ST141A" "J36F43S"       "P39K"          "P39QY42M"     
+# [5] "V36F"          "Y42FK108E"    
+print(levels(samplenames$SampleName))
+for ( i in seq(1, 6) ) {
+	# Assign variables tetRN, tetRN.av, and tetRN.lh
+	assign(paste('tetR', i, sep=''), subset(both.m, 
+			SampleName == levels(SampleName)[i]))
+	assign(paste('tetR', i, '.av', sep=''), subset(averages, 
+			SampleName == levels(SampleName)[i]))
+	assign(paste('tetR', i, '.lh', sep=''), subset(last_hour_averages, 
+			SampleName == levels(SampleName)[i]))
+}
+
+factor
+
+# Induction (or 'dynamics' plots)
+dyn_tetR1 = dynplot(tetR1, tetR1.av, "Conc. [ng/ml]", clpalette="Spectral")
+dyn_tetR2 = dynplot(tetR2, tetR2.av, "Conc. [ng/ml]", clpalette="Spectral")
+dyn_tetR3 = dynplot(tetR3, tetR3.av, "Conc. [ng/ml]", clpalette="Spectral")
+dyn_tetR4 = dynplot(tetR4, tetR4.av, "Conc. [ng/ml]", clpalette="Spectral")
+dyn_tetR5 = dynplot(tetR5, tetR5.av, "Conc. [ng/ml]", clpalette="Spectral")
+dyn_tetR6 = dynplot(tetR6, tetR6.av, "Conc. [ng/ml]", clpalette="Spectral")
+
+dosplot2 = function(lh_data, xunit='[µM]') {
+	maintitle = toString(unique(lh_data$SampleName))
+	xbreaks = unique(lh_data$Concentration)[-2]
+	p = qplot(Concentration, avg, data=lh_data, geom='line',
+		xlab = paste("Conc.", xunit), log = 'x', 
+		ylab="Normalised RFU at saturation [a.u]", main=maintitle,
+		ylim = c(0, max(lh_data$avg)))  +
+		geom_linerange(aes(ymin=avg-1.96*std, ymax=avg+1.96*std)) +
+		scale_x_continuous(breaks = xbreaks) +
+		scale_colour_hue("Repeat")
+	return(p)
+}
+
+dos_tetR1 = dospot2(tetR1.lh, xunit='[ng/ml]')
+dos_tetR2 = dosplot2(tetR2.lh, xunit='[ng/ml]')
+dos_tetR3 = dosplot2(tetR3.lh, xunit='[ng/ml]')
+dos_tetR4 = dosplot2(tetR4.lh, xunit='[ng/ml]')
+dos_tetR5 = dosplot2(tetR5.lh, xunit='[ng/ml]')
+dos_tetR6 = dosplot2(tetR6.lh, xunit='[ng/ml]')
+
+# Compare induction for each mutant at 0 ATC
+ATC0 = subset(both.m, Concentration == '0')
+p_atc0 = qplot(Time, NormRFU, data = ATC0, colour = SampleName,
+		xlab = 'Time', ylab = 'Normalised fluorescence [a.u.]',
+		main = 'TetR mutants, no ATC') +
+	scale_x_datetime(minor = '10 min', format = '%H h') +
+	scale_colour_brewer("Mutant", palette="Accent")
+
+# Same but for 2000 ATC
+ATC2000 = subset(both.m, Concentration == '2000')
+p_atc2000 = qplot(Time, NormRFU, data = ATC2000, colour = SampleName,
+		xlab = 'Time', ylab = 'Normalised fluorescence [a.u.]',
+		main = 'TetR mutants, 2000 ng/ml ATC') +
+	scale_x_datetime(minor = '10 min', format = '%H h') +
+	scale_colour_brewer("Mutant", palette="Accent")
+
+######################################
+# PNG exports
+######################################
+
+# The first plots I made
+for ( i in seq(1, 6) ) {
+	ggsave(paste("plots/nadine-exp", i, "-induction.png", sep=''), 
+		plot=get(paste('dyn_exp', i, sep='')),
+		width=12, height=7.5)
+	ggsave(paste("plots/nadine-exp", i, "-doseresponse.png", sep=''), 
+		plot=get(paste('dos_exp', i, sep='')),
+		width=12, height=7.5)
+}
+
+# TetR mutants
+
+for ( i in seq(1, 6) ) {
+ 	ggsave(paste("plots/tetR-", get(paste("tetR", i, sep=''))$SampleName[1],
+		"-induction.png", sep=""),
+		plot=get(paste('dyn_tetR', i, sep='')),
+		width=12, height=7.5)
+ 	ggsave(paste("plots/tetR-", get(paste("tetR", i, sep=''))$SampleName[1],
+		"-doseresponse.png", sep=""),
+		plot=get(paste('dos_tetR', i, sep='')),
+		width=12, height=7.5)
+}
+
+ggsave("plots/tetR-ATC0-induction.png", plot=p_atc0, width=12, height=7.5)
+ggsave("plots/tetR-ATC2000-induction.png", plot=p_atc2000, width=12, height=7.5)
--- plate-reader/plate-reader.R
+++ plate-reader/plate-reader.R
@@ -75,18 +75,18 @@ ggsave("plots/lysis_dynamics.pdf", width=16, height=10)
 
 # Average over the six measurements in the last hour. Keep repeats separate.
 merged$Hours = as.integer(format(merged$Time, "%H"))
-last_hour = merged[merged$Hours == max(merged$Hours),]
+last_hour = merged[merged$Hours == c(11,12),]
 dose_response = ddply(last_hour, .variables=c('SampleName', 'Concentration', 
-		'Repeat'), .fun=summarise, hourly_avg=mean(value), std=sd(value)/sqrt(6))
+		'Repeat'), .fun=summarise, hourly_avg=mean(value), std=sd(value)/sqrt(length(value)))
 
 # Average the repeats
 dose_response_avg = ddply(last_hour, .variables=c('SampleName', 
 		'Concentration'), 
-	.fun=summarise, avg=mean(value), std=sd(value)/sqrt(6*3))
+	.fun=summarise, avg=mean(value), std=sd(value)/sqrt((length(value))))
 
 # Again, points for each repeat, then a line as general average
-dose_response$SampleName = factor(dose_response$SampleName)
-dose_response_avg$SampleName = factor(dose_response_avg$SampleName)
+dose_response$SampleName = factor(dose_response$SampleName,labels=c("T7-Control plasmid","T7-Lysis plasmid","T7-RFP plasmid"))
+dose_response_avg$SampleName = factor(dose_response_avg$SampleName,labels=c("T7-Control plasmid","T7-Lysis plasmid","T7-RFP plasmid"))
 p_dose_response = qplot(Concentration, hourly_avg, data=dose_response, 
 	colour=SampleName, geom='point', xlab="IPTG concentration [µM]", 
 	ylab="Optical Density [a.u.]") +
@@ -94,103 +94,6 @@ p_dose_response = qplot(Concentration, hourly_avg, data=dose_response,
 	geom_line(aes(y=avg), data=dose_response_avg)
 ggsave("plots/dose-response.pdf", width=16, height=10)
 
-################################
-# Nadine's experiments
-################################
-
-od = read.table('data/16.09-nadine-OD.dat', header=TRUE, sep='\t')
-rfu = read.table('data/16.09-nadine-RFU.dat', header=TRUE, sep='\t')
-samplenames = read.table('config/16.09-nadine.titles', header=TRUE, sep='\t')
-
-# Melt and merge, convert time, insert sample concentration and name
-od.m = melt(od, id=c('Time'), variable_name='Code')
-rfu.m = melt(rfu, id=c('Time'), variable_name='Code')
-both.m = data.frame(Time=od.m$Time, Code=od.m$Code, OD=od.m$value,
-	RFU=rfu.m$value)
-both.m$Time = as.POSIXct(sapply(both.m$Time, toString), format="%H:%M:%S")
-both.m = merge(both.m, samplenames)
-
-# Normalise RFU, calculate average over the last hour (keep repeats separate)
-both.m$NormRFU = with(both.m, RFU/OD)
-
-# calculate last hour averages, and repeat averages
-averages = ddply(both.m, .variables=c('Time', 'SampleName', 'Concentration'),
-	.fun=summarise, avg=mean(NormRFU), std=sd(NormRFU)/sqrt(2))
-both.m$Hours = as.integer(format(both.m$Time, "%H"))
-last_hour_averages = ddply(subset(both.m, Hours == max(Hours) - 1), 
-		.variables=c('SampleName', 'Concentration', 'Repeat'), .fun=summarise,
-		avg=mean(NormRFU), std=sd(NormRFU)/sqrt(6))
-
-# Now separate experiments
-# Current numbering: (output of levels(samplenames$SampleName))
-# [1] "J61002 Plac-RFP + IPTG"      
-# [2] "J61002 Ptet-RFP + ATC"        
-# [3] "J6-pLac-RFP in BL21 + IPTG"      
-# [4] "pSB3K1-pConst-TetR + J6-pTet-RFP + ATC"                      
-# [5] "pSB3K1 Pconst-TetR Ptet-LacI + J61002 Plac-RFP + ATC"   
-# [6] "pSB3K1 Pconst-TetR Ptet-LacI + J61002 Plac-RFP + IPTG"         
-
-print(levels(samplenames$SampleName))
-for ( i in seq(1, 6) ) {
-	# Assign variables expN, expN.av, and expN.lh
-	assign(paste('exp', i, sep=''), subset(both.m, 
-			SampleName == levels(SampleName)[i]))
-	assign(paste('exp', i, '.av', sep=''), subset(averages, 
-			SampleName == levels(SampleName)[i]))
-	assign(paste('exp', i, '.lh', sep=''), subset(last_hour_averages, 
-			SampleName == levels(SampleName)[i]))
-}
-
-# Dynamics plots:
-dynplot = function(all_data, av_data, colourlabel, clpalette='YlOrRd') {
-	maintitle = toString(unique(all_data$SampleName))
-	all_data$Concentration = factor(all_data$Concentration)
-	av_data$Concentration = factor(av_data$Concentration)
-	dyn = qplot(Time, NormRFU, data = all_data, geom = 'point',
-		colour = Concentration,
-		xlab = "Time", ylab = "Normalised RFU [a.u.]", main=maintitle) +
-		geom_smooth() +
-		scale_colour_brewer(colourlabel, palette = clpalette) + 
-		scale_x_datetime(minor = '10 min', format = '%H h')
-	return(dyn)
-}
-
-# Dynamics and dose response plot for exp2:
-dosplot = function(lh_data, xunit='[µM]') {
-	maintitle = toString(unique(lh_data$SampleName))
-	xbreaks = unique(lh_data$Concentration)[-2]
-	p = qplot(Concentration, avg, data=lh_data, geom='point',
-		colour=factor(Repeat),
-		xlab = paste("Conc.", xunit), log = 'x', 
-		ylab="Normalised RFU at saturation [a.u]", main=maintitle )  +
-		geom_linerange(aes(ymin=avg-1.96*std, ymax=avg+1.96*std)) +
-		scale_x_continuous(breaks = xbreaks) +
-		scale_colour_hue("Repeat")
-	return(p)
-}
-
-# Make both plots for all experiments
-dyn_exp1 = dynplot(exp1, exp1.av, "Conc. [µM]")
-dyn_exp2 = dynplot(exp2, exp3.av, "Conc. [ng / ml]", clpalette="Spectral")
-dyn_exp3 = dynplot(exp3, exp2.av, "Conc. [µM]")
-dyn_exp4 = dynplot(exp4, exp3.av, "Conc. [ng / ml]", clpalette="Spectral")
-dyn_exp5 = dynplot(exp5, exp4.av, "Conc. [ng / ml]")
-dyn_exp6 = dynplot(exp6, exp5.av, "Conc. [µM]")
-
-dos_exp1 = dosplot(exp1.lh)
-dos_exp2 = dosplot(exp2.lh, xunit='[ng/ml]')
-dos_exp3 = dosplot(exp3.lh)
-dos_exp4 = dosplot(exp4.lh, xunit='[ng/ml]')
-dos_exp5 = dosplot(exp5.lh, xunit='[ng/ml]')
-dos_exp6 = dosplot(exp6.lh)
-
-# And save them
-for ( i in seq(1, 6) ) { 
-	ggsave(paste("plots/nadine-exp", i, "-dynamics.pdf", sep=''), 
-		plot=get(paste('dyn_exp', i, sep='')), width=16, height=10)
-	ggsave(paste("plots/nadine-exp", i, "-doseresponse.pdf", sep=''), 
-		plot=get(paste('dos_exp', i, sep='')), width=16, height=10)
-}
 
 ###############################
 # Non-random variants
@@ -212,12 +115,7 @@ normalised.m = data.frame(
 normalised.m = merge(normalised.m, samplenames)
 
 # Extract only the last hour (saturation), and average over those points
-normalised.m$Hours = as.integer(format(normalised.m$Time, "%H"))
-last_hour = normalised.m[normalised.m$Hours == max(normalised.m$Hours),]
-last_hour$Concentration = factor(last_hour$Concentration)
-last_hour_avg = ddply(last_hour, 
-	.variables=c('SampleName', 'Variant', 'Strength', 'Concentration'), 
-	.fun=summarise, avg=mean(norm_rfu), std=sd(norm_rfu)/sqrt(18))
+c
 
 # Order the labels by affinity (RFU at 250 uM IPTG) [NO LONGER USED]
 # last_250 = last_hour_avg[last_hour_avg$Concentration == 250, ]
@@ -254,7 +152,7 @@ last_hour.c = cast(last_hour.m,
 last_hour.c$InductionRatio = with(last_hour.c, IPTG250/IPTG0)
 
 ratio_averages = ddply(last_hour.c, .variables=c('Variant', 'Strength'),
-	.fun=summarise, avg=mean(InductionRatio), std=sd(InductionRatio)/sqrt(3))
+	.fun=summarise, avg=mean(InductionRatio), std=sd(InductionRatio)/sqrt(length(InductionRatio)))
 
 ratio_averages$Variant = ordered(ratio_averages$Variant,
 	levels=c("Without lac operator", "With lac operator"))
@@ -287,17 +185,81 @@ rand_norm.m = data.frame(
 # Compare designed and random mutants on a boxplot
 normalised.m$type = "Designed"
 rand_norm.m$type = "Random"
+
+normalised.m$Hours = as.integer(format(normalised.m$Time, "%H"))
+last_hour = normalised.m[normalised.m$Hours == c(11,12),]
+last_hour$Concentration = factor(last_hour$Concentration)
+last_hour_avg = ddply(last_hour, 
+  .variables=c('SampleName', 'Variant', 'Strength', 'Concentration'), 
+	.fun=summarise, avg=mean(norm_rfu), std=sd(norm_rfu)/sqrt(length(norm_rfu)))
+
+rand_norm.m$Hours = as.integer(format(rand_norm.m$Time, "%H"))
+last_hour_rand = rand_norm.m[rand_norm.m$Hours == c(11,12),]
+#last_hour_rand$Concentration = factor(last_hour_rand$Concentration)
+last_hour_rand_avg = ddply(last_hour_rand, 
+  .variables=c('Code'), 
+  .fun=summarise, avg=mean(norm_rfu), std=sd(norm_rfu)/sqrt(length(norm_rfu)))
+
+last_hour_avg$type="Designed"
+last_hour_rand_avg$type="Random"
+
+both=merge(last_hour_avg[last_hour_avg$Concentration=="250",],last_hour_rand_avg,all=T)
+
 both = merge(normalised.m, rand_norm.m, all=T)
 
-p_var_comparison = qplot(type, norm_rfu, data = both, geom = 'boxplot', 
+p_var_comparison = qplot(type, avg, data = both, geom = 'boxplot', 
 	binwidth=50, position='dodge', ylab="Normalised RFU [a.u.]", xlab=NULL, 
 	  main="Variability in promoter efficiency")
 ggsave("plots/variability_comparison.pdf", width=3, height=7)
 
+
+
 ######################################
-# DNA recovery experiment
+# DNA recovery experiment : First experiments
 ######################################
 
+#OD
+
+firstOD=read.table("data/first_dnarecov_OD.dat",header=TRUE)
+firstOD=melt(firstOD,id.vars="Time")
+plot.firstOD=qplot(Time, value, data=firstOD, geom='line',
+      colour=variable,group=variable,
+      xlab = 'Time [hours]', ylab = 'OD 600') +
+  scale_colour_hue(h=c(0,120))
+
+#DNA in supernatant
+
+DNAsuper=read.table("data/first_dnarecov_DNAsupernatant.dat",header=TRUE)
+DNAsuper=melt(DNAsuper,id.vars="Time")
+plot.DNAsuper=qplot(Time, value, data=DNAsuper, geom='line',
+      colour=variable,group=variable,
+      xlab = 'Time [hours]', ylab = 'mRFP1 genes in supernatant [pM]') +
+  scale_colour_hue(h=c(0,120))
+
+#CFU 
+
+firstCFU=read.table("data/first_dnarecov_CFU.dat",header=TRUE)
+firstCFU=melt(firstCFU,id.vars="Time")
+plot.firstcfu=qplot(Time, value, data=firstCFU, geom='line',
+      colour=variable,group=variable,
+      xlab = 'Time [hours]', ylab = 'Colony forming units per uL') +
+  scale_colour_hue(h=c(0,120))
+
+######################################
+# DNA recovery experiment : Second experiments
+######################################
+
+#OD :
+OD_lysing=read.table("data/17.09-dnarecov-OD.dat",header=TRUE)
+colnames(OD_lysing)=c("Time","Lysing RFP","Lysing GFP")
+OD_lysing.m=melt(OD_lysing)
+
+p.OD_lysing=qplot(Time, value, data=OD_lysing.m, geom='line',
+      colour=variable,group=variable,
+      xlab = 'Time [hours]', ylab = 'OD 600') +
+  scale_colour_hue(h=c(0,120))
+
+#Fluorescence :
 rfp = read.table('data/17.09-dnarecov-RFP.dat', header=TRUE, sep='\t')
 gfp = read.table('data/17.09-dnarecov-GFP.dat', header=TRUE, sep='\t')
 
@@ -305,8 +267,8 @@ rfp.m = melt(rfp, id=c('Hour'), variable_name='Sample')
 gfp.m = melt(gfp, id=c('Hour'), variable_name='Sample')
 
 # Rename 'value' column to RFU or GFU
-names(rfp.m)[3] = 'RFU'
-names(gfp.m)[3] = 'GFU'
+names(rfp.m)[3] = 'RFP'
+names(gfp.m)[3] = 'GFP'
  
 # merge and melt again, clean up sample name
 dna_recovery = merge(rfp.m, gfp.m)
@@ -315,12 +277,47 @@ dna_recovery$Sample = factor(dna_recovery$Sample,
 dna_recovery.m = melt(dna_recovery, id=c('Hour', 'Sample'), 
 	variable_name='Measure')
 
+dna_recovery.norm=ddply(.data=dna_recovery.m,.variables=c("Measure","Sample"),.fun=transform,norm_value=value/value[1])
 # Plot comparison of two output colours, for both samples
 
-qplot(Hour, value, data = dna_recovery.m, geom = 'point',
-	colour = Sample,
+plot_lysis_r_g_fp=qplot(Hour, norm_value, data = dna_recovery.norm, geom = 'line',
+	colour = Measure,
 	xlab = 'Time [hours]', ylab = 'Fluorescence [a.u.]') +
-	facet_grid(Measure ~ ., scales = 'free_y')
+	facet_grid(Sample ~ .) +
+  scale_colour_hue(h=c(0,120)) 
+    
+#Colony :
+                         
+RFP.colonies=read.table("data/17.09-dnarecov-colony_C2RFP.dat",header=TRUE)
+GFP.colonies=read.table("data/17.09-dnarecov-colony_C2GFP.dat",header=TRUE)
+RFP.colonies$Sample=rep("Lysing RFP",nrow(RFP.colonies))
+GFP.colonies$Sample=rep("Lysing GFP",nrow(GFP.colonies))
+colonies=rbind(melt(RFP.colonies),melt(GFP.colonies))
+
+
+plot.CFU=qplot(Time, value, data = colonies, geom = 'line',
+  colour = variable, group=variable,
+	xlab = 'Time [hours]', ylab = 'Colony forming units per uL') +
+	facet_grid(Sample ~ ., scale="free_y") +
+  scale_colour_hue(h=c(120,0))      
+
+
+#qPCR
+
+RFP.qpcr=read.table("data/17.09-dnarecov-qpcr_C2RFP.dat",header=TRUE)
+GFP.qpcr=read.table("data/17.09-dnarecov-qpcr_C2GFP.dat",header=TRUE)
+RFP.qpcr$Sample=rep("Lysing RFP",nrow(RFP.qpcr))
+GFP.qpcr$Sample=rep("Lysing GFP",nrow(GFP.qpcr))
+qPCR=rbind(melt(RFP.qpcr,id.vars=c("Time","Sample")),melt(GFP.qpcr,id.vars=c("Time","Sample")))
+
+
+plot.qPCR=qplot(Time, value, data = qPCR, geom = 'line',
+  colour = variable, group=variable,
+  xlab = 'Time [hours]', ylab = 'genes in supernatant, pM') +
+	facet_grid(Sample ~ ., scale="free_y") +
+  scale_colour_hue(h=c(0,120))      
+
+
 
 ######################################
 # PNG exports
@@ -349,14 +346,10 @@ ggsave("plots/non_random_dose_response.png", plot=p_non_random,
 	width=12, height=7.5)
 ggsave("plots/varability_comparison.png", plot=p_var_comparison, 
 	width=4, height=10)
-
-# Nadine's stuff
-
-for ( i in seq(1, 6) ) {
-	ggsave(paste("plots/nadine-exp", i, "-induction.png", sep=''), 
-		plot=get(paste('dyn_exp', i, sep='')),
-		width=12, height=7.5)
-	ggsave(paste("plots/nadine-exp", i, "-doseresponse.png", sep=''), 
-		plot=get(paste('dos_exp', i, sep='')),
-		width=12, height=7.5)
-}
+ggsave("plots/rgfp_lysis_comparison.png",plot=plot_lysis_r_g_fp,width=8,height=8)
+ggsave("plots/ODlysis.png",plot=p.OD_lysing,width=8,height=6)
+ ggsave("plots/CFU.png",plot=plot.CFU,width=8,height=8)       
+ggsave("plots/qPCR_dnarecovery.png",plot=plot.qPCR,width=8,height=8)
+ggsave("plots/first_dnarecov_OD.png",plot=plot.firstOD,width=8,height=6)
+ggsave("plots/first_DNA_supernatant.png",plot=plot.DNAsuper,width=8,height=6)
+ggsave("plots/firstCFU.png",plot=plot.firstcfu,width=8,height=6)
